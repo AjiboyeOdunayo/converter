d0e512a1fcea4ec61e2c06140b87b77ac69a2d67
AjiboyeOdunayo <odunajiboy@gmail.com>
2020-06-18 01:49:21 +0100

    init

--- convert.js
+++ convert.js
@@ -1,3 +1,37 @@
+// 	var result = document.getElementById("result");
+// 	var result2 = document.getElementById("result2");
+
+//     input.addEventListener("keyup",myResult);
+//     result.addEventListener("keyup",myResult);
+//      result2.addEventListener("keyup",myResult);
+
+// function myResult(){
+// 	 //result.value = input.value;
+// 	 //input.value = result.value;
+
+//      if (/*result.value = */input.value != "" ) {  
+//  		result.value = input.value *0.01;
+
+//  		var result2 = document.getElementById("result2").innerHTML
+//       = input.value + " CM = " +  result.value + " M "
+
+//      } else if (result.value != "" /*input.value*/) {
+
+//          input.value = result.value * 100;
+//          var result2 = document.getElementById("result2").innerHTML
+//              = result.value + " M = " + input.value + " CM "
+
+//  	}
+//  };
+
+
+
+
+
+
+
+
+
 	var input = document.getElementById("input");
 	var result = document.getElementById("result");
 	var result2 = document.getElementById("result2");
@@ -10,21 +44,29 @@ function myResult(){
 	// result.value = input.value;
 	// input.value = result.value;
 
-     if (result.value = input.value ) {  
+     if  (result.value = input.value) {  
  		result.value = input.value *0.01;
 
  		var result2 = document.getElementById("result2").innerHTML
-      = input.value + " CM = " +  result.value + " M "
+      = input.value + " CM=" +  result.value + " M "
 
- 	}else if(input.value = result.value) {
+ 	// }else if(input.value = result.value) {
 
- 		input.value = result.value *100;
+ 	// 	input.value = result.value *100;
  	
+
+
+//  	function myResult(){
+// 	// result.value = input.value;
+// 	// input.value = result.value;
+
+//      if  (input.value = result.value) {  
+//  		input.value = result.value *100;
+// } 	
  	}
- };
+ 	 }
+ 	
 
- 
-
-
+ 	 
 
 
