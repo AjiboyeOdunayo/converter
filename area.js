b1719eddcad067e32e48236b5281e06df7b0f447
AjiboyeOdunayo <odunajiboy@gmail.com>
2020-06-22 01:45:30 +0100

    init

--- area.js
+++ area.js
@@ -1,4 +1,4 @@
-// Acre convertion
+// Acre convertion start here.
      var input1 = document.getElementById("input1");
      var input2 = document.getElementById("input2");
      var input3 = document.getElementById("input3");
@@ -11,7 +11,7 @@
 
 
     input1.addEventListener("keyup",acre);
-    input2.addEventListener("keyup",acre);
+    // input2.addEventListener("keyup",acre);
     
 
     function acre(){
@@ -25,51 +25,46 @@
       input9.value = input1.value * 4840;
 
      }
-
-
-
-    //  var input2 = document.getElementById("input2");
-    //  var input1 = document.getElementById("input1");
-    //  var input3 = document.getElementById("input3");
-    //  var input4 = document.getElementById("input4");
-    //  var input5 = document.getElementById("input5");
-    //  var input6 = document.getElementById("input6");
-    //  var input7 = document.getElementById("input7");
-    //  var input8 = document.getElementById("input8");
-    //  var input9 = document.getElementById("input9");
-
-
-    // input2.addEventListener("keyup",hectConvert);
-    // input1.addEventListener("keyup",hectConvert);
-
-    // function hectConvert(){
-    //           input1.value = input2.value  * 2.4710538147;
-    //           input3.value = input2.value * 100000000;
-    //           input4.value = input2.value * 107639.10417;
-    //           input5.value = input2.value  * 15500031;
-    //           input6.value = input2.value / 100;
-    //           input7.value = input2.value * 10000;
-    //           input8.value = input2.value / 258.998811;
-    //           input9.value = input2.value * 11959.900463;
-
-
-    //          }
-
-// square centimeter conversation start from here
-
-     var input3 = document.getElementById("input3");
-     var input1 = document.getElementById("input1");
+// Acre convertion stop here.
+     
      var input2 = document.getElementById("input2");
+     var input1 = document.getElementById("input1");
      var input3 = document.getElementById("input3");
      var input4 = document.getElementById("input4");
      var input5 = document.getElementById("input5");
      var input6 = document.getElementById("input6");
      var input7 = document.getElementById("input7");
      var input8 = document.getElementById("input8");
+     var input9 = document.getElementById("input9");
+
+
+    input2.addEventListener("keyup",sqHectare);
+
+     function sqHectare(){
+      input1.value = input2.value * 2.4710538147;
+      input3.value = input2.value  * 100000000;
+      input4.value = input2.value * 107639.10417;
+      input5.value = input2.value * 15500031;
+      input6.value = input2.value / 100;
+      input7.value = input2.value * 10000;
+      input8.value = input2.value / 258.998811;
+      input9.value = input2.value * 11959.900463;
+     }
+
+// square centimeter conversation start from here
+
+     var input3 = document.getElementById("input3");
+     var input1 = document.getElementById("input1");
+     var input2 = document.getElementById("input2");
+     var input4 = document.getElementById("input4");
+     var input5 = document.getElementById("input5");
+     var input6 = document.getElementById("input6");
+     var input7 = document.getElementById("input7");
+     var input8 = document.getElementById("input8");
+     var input9 = document.getElementById("input9");
 
 
     input3.addEventListener("keyup",sqCent);
-    // input2.addEventListener("keyup",area);
 
      function sqCent(){
       input1.value = input3.value / 40468564.224;
@@ -81,3 +76,168 @@
       input8.value = input3.value / 2589988.1103;
       input9.value = input3.value / 8361.2736;
      }
+// square centimeter conversation stop from here
+
+
+// square feet conversation start from here
+     var input4 = document.getElementById("input4");
+     var input1 = document.getElementById("input1");
+     var input2 = document.getElementById("input2");
+     var input3 = document.getElementById("input3");
+     var input5 = document.getElementById("input5");
+     var input6 = document.getElementById("input6");
+     var input7 = document.getElementById("input7");
+     var input8 = document.getElementById("input8");
+     var input9 = document.getElementById("input9");
+
+
+    input4.addEventListener("keyup",sqFeet);
+
+    function sqFeet(){
+      input1.value = input4.value / 43560;
+      input2.value = input4.value / 107639.10417;
+      input3.value = input4.value * 929.0304;
+      input5.value = input4.value * 144;
+      input6.value = input4.value / 10763910.417;
+      input7.value = input4.value / 10763910.417;
+      input8.value = input4.value / 27878400;
+      input9.value = input4.value / 9;
+     }
+// square feet conversation stop from here
+
+// square inch conversation satrt from here
+     var input5 = document.getElementById("input5");
+     var input1 = document.getElementById("input1");
+     var input2 = document.getElementById("input2");
+     var input3 = document.getElementById("input3");
+     var input4 = document.getElementById("input4");
+     var input6 = document.getElementById("input6");
+     var input7 = document.getElementById("input7");
+     var input8 = document.getElementById("input8");
+     var input9 = document.getElementById("input9");
+
+
+    input5.addEventListener("keyup",sqInch);
+
+     function sqInch(){
+      input1.value = input5.value / 6272640;
+      input2.value = input5.value / 15500031;
+      input3.value = input5.value * 6.4516;
+      input4.value = input5.value / 144;
+      input6.value = input5.value/ 1550003100;
+      input7.value = input5.value / 1550.0031;
+      input8.value = input5.value / 4014489599.9;
+      input9.value = input5.value  / 1296;
+     }
+
+// square inch conversation stop from here
+     
+// square kilometer conversation satrt from here
+     var input6 = document.getElementById("input6");
+     var input1 = document.getElementById("input1");
+     var input2 = document.getElementById("input2");
+     var input3 = document.getElementById("input3");
+     var input4 = document.getElementById("input4");
+     var input5 = document.getElementById("input5");
+     var input7 = document.getElementById("input7");
+     var input8 = document.getElementById("input8");
+     var input9 = document.getElementById("input9");
+
+
+    input6.addEventListener("keyup",sqKm);
+
+     function sqKm(){
+      input1.value = input6.value * 247.105381476272640;
+      input2.value = input6.value  * 100;
+      input3.value = input6.value * 10000000000;
+      input4.value = input6.value * 10763910.417;
+      input5.value = input6.value* 1550003100;
+      input7.value = input6.value  * 1000000;
+      input8.value = input6.value / 2.58998811;
+      input9.value = input6.value   * 1195990.0463;
+     }
+
+// square kilometer conversation stop from here
+
+ // square meter conversation satrt from here
+
+     var input7 = document.getElementById("input7");
+     var input1 = document.getElementById("input1");
+     var input2 = document.getElementById("input2");
+     var input3 = document.getElementById("input3");
+     var input4 = document.getElementById("input4");
+     var input5 = document.getElementById("input5");
+     var input6 = document.getElementById("input6");
+     var input8 = document.getElementById("input8");
+     var input9 = document.getElementById("input9");
+
+
+    input7.addEventListener("keyup",sqMeter);
+
+     function sqMeter(){
+      input1.value = input7.value / 4046.8564224;
+      input2.value = input7.value / 10000;
+      input3.value = input7.value * 10000;
+      input4.value = input7.value * 10.763910417;
+      input5.value = input7.value * 1550.0031;
+      input6.value = input7.value * 1000000;
+      input8.value = input7.value / 2589988.1103;
+      input9.value = input7.value * 1.19599005;
+     }
+// square meter conversation stop from here
+     
+// square mile conversation satrt from here
+
+     var input8 = document.getElementById("input8");
+     var input1 = document.getElementById("input1");
+     var input2 = document.getElementById("input2");
+     var input3 = document.getElementById("input3");
+     var input4 = document.getElementById("input4");
+     var input5 = document.getElementById("input5");
+     var input6 = document.getElementById("input6");
+     var input7 = document.getElementById("input7");
+     var input9 = document.getElementById("input9");
+
+
+    input8.addEventListener("keyup",sqMile);
+
+     function sqMile(){
+      input1.value = input8.value * 640;
+      input2.value = input8.value * 258.998811;
+      input3.value = input8.value * 25899881103;
+      input4.value = input8.value * 27878400;
+      input5.value = input8.value * 4014489599.9;
+      input6.value = input8.value * 2.58998811;
+      input7.value = input8.value * 2589988.1103;
+      input9.value = input8.value * 3097600;
+     }
+// square mile conversation stop from here
+
+// square yard conversation satrt from here
+
+     var input9 = document.getElementById("input9");
+     var input1 = document.getElementById("input1");
+     var input2 = document.getElementById("input2");
+     var input3 = document.getElementById("input3");
+     var input4 = document.getElementById("input4");
+     var input5 = document.getElementById("input5");
+     var input6 = document.getElementById("input6");
+     var input7 = document.getElementById("input7");
+     var input8 = document.getElementById("input8");
+
+
+    input9.addEventListener("keyup",sqYard);
+
+     function sqYard(){
+      input1.value = input9.value  / 4840;
+      input2.value = input9.value  / 11959.900463;
+      input3.value = input9.value  * 8361.2736;
+      input4.value = input9.value  * 9;
+      input5.value = input9.value  * 1296;
+      input6.value = input9.value  / 1195990.0463;
+      input7.value = input9.value  / 1.19599005;
+      input8.value = input9.value  / 3097600;
+     }
+// square yard conversation stop from here
+     
+     
--- convert.js
+++ convert.js
@@ -1,70 +1,33 @@
-// 	var result = document.getElementById("result");
-// 	var result2 = document.getElementById("result2");
-
-//     input.addEventListener("keyup",myResult);
-//     result.addEventListener("keyup",myResult);
-//      result2.addEventListener("keyup",myResult);
-
-// function myResult(){
-// 	 //result.value = input.value;
-// 	 //input.value = result.value;
-
-//      if (/*result.value = */input.value != "" ) {  
-//  		result.value = input.value *0.01;
-
-//  		var result2 = document.getElementById("result2").innerHTML
-//       = input.value + " CM = " +  result.value + " M "
-
-//      } else if (result.value != "" /*input.value*/) {
-
-//          input.value = result.value * 100;
-//          var result2 = document.getElementById("result2").innerHTML
-//              = result.value + " M = " + input.value + " CM "
-
-//  	}
-//  };
-
-
-
-
-
-
-
-
-
-	var input = document.getElementById("input");
+  var input = document.getElementById("input");
 	var result = document.getElementById("result");
 	var result2 = document.getElementById("result2");
 
     input.addEventListener("keyup",myResult);
-    result.addEventListener("keyup",myResult);
-    // result2.addEventListener("keyup",myResult);
     
 function myResult(){
  		result.value = input.value *0.01;
 
  		var result2 = document.getElementById("result2").innerHTML
-      = input.value + " CM=" +  result.value + " M "
+      = input.value + " CM  =   " +  result.value + " M "
 
  	}
 
 
+  var result = document.getElementById("result");
+  var input = document.getElementById("input");
 
- // 	var result = document.getElementById("result");
-	// var input = document.getElementById("input");
-	// var result2 = document.getElementById("result2");
+  result.addEventListener("keyup",myMeter);
 
- //    result.addEventListener("keyup",myCent);
- //     input.addEventListener("keyup",myCent);
- 	 
- // 	function myCent(){
- // 		input.value = result.value *100;
+  function myMeter(){
+    input.value = result.value *100
 
- 		// var result2 = document.getElementById("result2").innerHTML
-   //    = input.value + " CM=" +  result.value + " M "
+    var result2 = document.getElementById("result2").innerHTML
+      = result.value + " CM  =   " +  input.value + " M "
 
- 	// }
+}
 
- 	 
+
+
+ 	
 
 
